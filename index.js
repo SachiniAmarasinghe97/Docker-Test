c6ac39820e2dcdebf23e3224249e189d94e60afb
Sachini Amarasinghe <sbamarasinghe97@gmail.com>
2024-10-20 02:15:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express()
+var port = 3000
+
+app.get("/", function(req,res){
+
+    res.send("Hello world!")
+
+});
+
+app.listen(port, function(){
+    console.log("Server started on port 3000!")
+})
